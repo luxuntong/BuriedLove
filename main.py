8a75a36245680058e0d3890f310893943efd0644
shanlihou <shanlihou@gmail.com>
2018-10-23 11:21:11 +0800

    base

--- main.py
+++ main.py
@@ -2,35 +2,36 @@
 import asyncio
 import threading
 from mqClient import py
-from mqtt import MQTT
+from RPC import RPC
 
 from aiohttp import web
 from aiohttp import web_runner
 
+
 async def init(loop):
     app = web.Application(loop=loop)
     app = web_runner.AppRunner(app=app).app()
-    app.router.add_post('/cmd/', cmd, name='cmd', expect_handler=web.Request.json)
+    app.router.add_post('/cmd/', cmd, name='cmd',
+                        expect_handler=web.Request.json)
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 8000)
     print('Server started at http://127.0.0.1:8000...')
     return srv
 
-        
+
 async def cmd(request):
     data = await request.json()
-    print(data)
-    print(MQTT().GPSPools)
-    if 'name' not in data:
-        return web.json_response({'error': '"name" is a required field'})
+    RPC().parseCmd(data)
+    return web.json_response({'result': 'success'})
 
-    
+
 def start():
     loop = asyncio.get_event_loop()
     tasks = [init(loop)]
     loop.run_until_complete(asyncio.wait(tasks))
     loop.run_forever()
 
+
 if __name__ == '__main__':
     t = threading.Thread(target=py.client_loop, args=('hh',))
-    t.start()    
+    t.start()
     start()
--- mqtt.py
+++ mqtt.py
@@ -1,13 +1,8 @@
+# coding=utf-8
 import time
 import heapq
-def singleton(cls, *args, **kw):
-    instances = {}
+from singleton import singleton
 
-    def _singleton():
-        if cls not in instances:
-            instances[cls] = cls(*args, **kw)
-        return instances[cls]
-    return _singleton
 
 class GPS(object):
     def __init__(self, jsonData):
@@ -20,34 +15,41 @@ class GPS(object):
         self.altitude = jsonData['altitude']
         self.speed = jsonData['speed']
         self.direction = jsonData['direction']
-        
+
     def __lt__(self, other):
         return self.timestamp < other.timestamp
-        
+
+
+# 单个设备的所有GPS信息池
 class GPSPool(list):
     def __init__(self, devId):
         self.devId = devId
-    
+
     def addData(self, data):
         heapq.heappush(self, GPS(data))
+
+    # 返回按照时间戳排序的gps信息队列
+    def getSortData(self):
+        return heapq.nsmallest(len(self), self)
+
 
 @singleton
 class MQTT(object):
     def __init__(self, *args, **kwargs):
         self.first = True
         self.GPSPools = {}
-    
+
     def setInfo(self, jsonData):
         devId = jsonData['devId']
         self.GPSPools.setdefault(devId, GPSPool(devId))
         self.GPSPools[devId].addData(jsonData)
-        
+
     def printDiff(self, latitude, longitude, timestamp):
         print(self.first)
         if self.first:
             return
-        
+
         print('-' * 60)
-        print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(timestamp)))
+        print(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(timestamp)))
         print(timestamp - self.timestamp)
         print(latitude - self.latitude, longitude - self.longitude)
